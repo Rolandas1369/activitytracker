5987b09486d767e90a7af42cb31d0a0434181a15
Rolandas1369 <rolandaswb@gmail.com>
2021-08-08 18:08:36 +0300

    nginx test

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('construction/', include('construction.urls')),
+    path('api/', include('construction.urls')),
     path('admin/', admin.site.urls),
 ]
