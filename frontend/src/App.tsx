00f07ac494180f1026f57cf2c950273f6666e381
Rolandas1369 <rolandaswb@gmail.com>
2021-08-19 11:29:40 +0300

    add typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,16 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and sassffvxxxe to reload.
         </p>
-        <p>sss</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn www Reaijjsssct
         </a>
       </header>
     </div>
