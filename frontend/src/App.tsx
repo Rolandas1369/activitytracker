90ef264996c70145824e08a22d4e3b53136e43ca
Rolandas1369 <rolandaswb@gmail.com>
2021-08-19 16:21:48 +0300

    add typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+
 
 function App() {
+
+  const [site, setSite] = useState([
+  {
+    name: "Zujunai",
+    location: "100.100",
+    price: 1000,
+    workers: ['Tomas', 'Viktoras']
+  },
+  {
+    name: "Zujunai",
+    location: "100.100",
+    price: 1000,
+    workers: ['Tomas', 'Viktoras']
+  },
+]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and sassffvxxsssssxe to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn www Reaijjsssct
-        </a>
-      </header>
+      <h1>Objektai</h1>
     </div>
   );
 }
