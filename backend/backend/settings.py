692c01b70e9d32456822bf4248fb7a6c35a3086d
Rolandas1369 <rolandaswb@gmail.com>
2021-12-27 10:21:56 +0200

    fr test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,9 +145,9 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'account.Account'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
