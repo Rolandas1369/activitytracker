af9d22269169abd7713cfe8d8975dabab2be000a
Rolandas1369 <rolandaswb@gmail.com>
2022-02-22 15:09:40 +0200

    login part

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -1,27 +1,42 @@
 import { useState, FunctionComponent } from "react";
-
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import { config } from "../Constants";
 
 const Login: FunctionComponent = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const [message, setMessage] = useState<null | string>(null);
 
   const createToken = () => {
-     axios
-      .post(config.url.API_URL + "token/", { email: username, password: password })
+    axios
+      .post(config.url.API_URL + "token/", {
+        email: username,
+        password: password,
+      })
       .then((res) => {
-        const data = res.data as {access: string}
-        const token  = data.access
-       localStorage.setItem('access_token', token)
-        console.log(res)
-      }).catch((er) => console.log(er))
+        if (res.status === 200) {
+          setRedirect(true);
+        }
+        const data = res.data as { access: string };
+        const token = data.access;
+        localStorage.setItem("access_token", token);
+        console.log(res);
+      })
+      .catch((er) => {
+        const msg = er as { message: string };
+        const mss = msg.message;
+        setMessage(mss);
+      });
   };
 
   return (
-    <div>
+    <div className="flex mt-48 justify-center">
+      {redirect ? <Redirect to="/homepage" /> : null}
+
       <form
-        className="bg-red-500"
+        className="flex flex-col h-full w-48 bg-red-500"
         action=""
         onSubmit={(e) => {
           e.preventDefault();
@@ -44,6 +59,7 @@ const Login: FunctionComponent = () => {
         ></input>
         <button type="submit">Login</button>
       </form>
+      {message}
     </div>
   );
 };
